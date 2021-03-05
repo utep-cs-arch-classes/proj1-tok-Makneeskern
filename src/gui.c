2ff2ed2ad372b41cffb985198151ddfaf41a4290
Michael Kneeskern <Makneeskern@miners.utep.edu>
2021-03-05 12:52:22 -0700

    Wrote the body of print_history in history.c and tested add_hstory. add_history didn't work as expected, so I reworked it until the result was what was expected. Deleted most of main in gui.c to start making it bahave more like the examples shown in class.

--- src/gui.c
+++ src/gui.c
@@ -6,22 +6,4 @@
 int main(void){
   int counter = 0;
   char *input = (char*) malloc(sizeof(char) * 100);
-
-  while(counter < 100 && (*(input + counter) = getchar()) != '\n'){
-    counter++;
-  }
-
-  *(input + counter+ 1) = '\0';
-  char **tokens = tokenize(input);
-
-  print_tokens(tokens);
-
-  List *history = init_history();
-  counter = 0;
-  while(*(tokens + counter) != 0){
-    add_history(history, *(tokens + counter));
-    counter++;
-  }
-
-  printf("Everything went off without a hitch...\nhopefully.");
 }
--- src/history.c
+++ src/history.c
@@ -4,31 +4,33 @@
 
 List* init_history(){
   List *history = (List*) malloc(sizeof(List));
-  history->root = NULL;
+  history->root = (Item*) malloc(sizeof(Item));
+  history->root->id = -1;
   return history;
 }
 
 void add_history(List *list, char *str){
-  if(list->root == NULL){
-    list->root = (Item*) malloc(sizeof(Item));
-    list->root->next = NULL;
-  }
   int counter = 0;
-  Item current_entry = *(list->root);
-  while(current_entry.next != NULL){
-    current_entry = *(current_entry.next);
+  Item *current_entry = list->root;
+  while(current_entry->id != -1){
+    current_entry = current_entry->next;
     counter++;
   }
-  current_entry.id = counter;
-  current_entry.str = str;
-  current_entry.next = (Item*) malloc(sizeof(Item));
-  current_entry.next->next = NULL;
+  current_entry->id = counter;
+  current_entry->str = str;
+  current_entry->next = (Item*) malloc(sizeof(Item));
+  current_entry->next->id = -1;
 }
 
 char *get_history(List *list, int id){
 }
 
 void print_history(List *list){
+  Item current_entry = *(list->root);
+  while(current_entry.next != NULL){
+    printf("%s\n", current_entry.str);
+    current_entry = *(current_entry.next);
+  }
 }
 
 void free_history(List *list){
